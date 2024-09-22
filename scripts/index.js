6abb42482bcd67c45ad40f47aa61777daa39c1d0
gospellevi <gospel4levi@gmail.com>
2024-09-23 00:55:19 +0100

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -23,8 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
        
     });
 
-    
-
 
     // Current Year and Last Modified
     const currentyear = document.querySelector("#currentyear");
@@ -40,21 +38,4 @@ document.addEventListener('DOMContentLoaded', () => {
         lastmodified.innerHTML = `Last Updated: ${document.lastModified}`;
     }
 
-    // Hamburger Menu
-    // const hamburger = document.createElement('button');
-    // hamburger.id = 'hamburger';
-    // hamburger.innerHTML = '&#9776;'; // hamburger icon
-
-    // const header = document.querySelector('header');
-    // const nav = document.getElementById('navMenu');
-    // if (header && nav) {
-    //     header.insertBefore(hamburger, nav);
-
-        // Toggle nav visibility when hamburger is clicked
-        // hamburger.addEventListener('click', () => {
-            // nav.classList.toggle('show'); // Toggle the 'show' class on the nav menu
-        //     hamburger.innerHTML = nav.classList.contains('show') ? '&#10006;' : '&#9776;'; // Toggle between hamburger and close icon
-        // });
-    // }
-    
 });
